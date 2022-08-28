381d02464a0bbd3d9899e3af17b775f69b97c922
cortinif <cortini.francesco@outlook.it>
2022-08-28 16:16:09 +0200

    return_to_zero

--- project_entry_point.py
+++ project_entry_point.py
@@ -1,11 +1,2 @@
 print('instruction_1')
-print('instruction_2')
-print('instruction_2')
-print('instruction_2')
-print('instruction_2')
-print('instruction_3')
-print('instruction_3')
-print('instruction_3')
-print('instruction_2')
-print('instruction_3')
-print('instruction_3')
+
